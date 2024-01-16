98da67052c5d7b91c09ac9194ccebc1777264b91
Fabrice Fulconis <ffulconis@prodware.fr>
2024-01-16 16:52:44 +0100

    Keep container after pipeline

--- Build-Pipelines/scripts/DevOps-Pipeline.ps1
+++ Build-Pipelines/scripts/DevOps-Pipeline.ps1
@@ -89,6 +89,7 @@ Run-AlPipeline @params `
     -CreateRuntimePackages:$CreateRuntimePackages `
     -appBuild $appBuild -appRevision $appRevision `
     -enableTaskScheduler:$enableTaskScheduler `
+    -keepContainer `
     -NewBcContainer {
         Param([Hashtable]$parameters)
         $parameters += @{ "dns" = "8.8.8.8" }
