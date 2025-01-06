21a00fb75f4599f5959e44e09ef35fb9bb6bc5a3
ProdwareNAVBCFrance <82646058+ProdwareNAVBCFrance@users.noreply.github.com>
2025-01-06 12:58:46 +0100

    Update UpdateEnv.ps1

--- Build-Pipelines/scripts/UpdateEnv.ps1
+++ Build-Pipelines/scripts/UpdateEnv.ps1
@@ -8,6 +8,7 @@ Write-Host $environmentName
 
 
 $aadAppRedirectUri = "http://localhost"                   # partner's AAD app redirect URI
+$OutPath = "$($PSScriptRoot)\bcSaasCustomers.json"
 
 $response = Invoke-RestMethod -Uri "https://frbc.blob.core.windows.net/bcsaascustomers/bcSaasCustomers.json?sp=r&st=2024-01-16T10:14:40Z&se=2030-01-01T18:14:40Z&spr=https&sv=2022-11-02&sr=b&sig=LcwDV1NdSlNJmKUgjEpRVPP98k%2Fa%2BDvNB52elt5632s%3D" -UseBasicParsing -ContentType "application/json" -OutFile $OutPath
 $tenants = Get-Content $OutPath -raw | Out-String | ConvertFrom-Json
