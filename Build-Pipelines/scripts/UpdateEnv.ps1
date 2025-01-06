726705d7870965016e48540561bd85f00bc27698
ProdwareNAVBCFrance <82646058+ProdwareNAVBCFrance@users.noreply.github.com>
2025-01-06 12:56:34 +0100

    Update UpdateEnv.ps1

--- Build-Pipelines/scripts/UpdateEnv.ps1
+++ Build-Pipelines/scripts/UpdateEnv.ps1
@@ -1,7 +1,6 @@
 $aadTenantId = {$env:aadTenantId}
 $newEnvironmentName = {$env:newEnvironmentName}
 $environmentName = {$env:environmentName}
-$bcSaasCustomers = {$env:bcSaasCustomers}
 
 Write-Host $aadTenantId
 Write-Host $newEnvironmentName
@@ -10,7 +9,7 @@ Write-Host $environmentName
 
 $aadAppRedirectUri = "http://localhost"                   # partner's AAD app redirect URI
 
-$response = Invoke-RestMethod -Uri "$bcSaasCustomers" -UseBasicParsing -ContentType "application/json" -OutFile $OutPath
+$response = Invoke-RestMethod -Uri "https://frbc.blob.core.windows.net/bcsaascustomers/bcSaasCustomers.json?sp=r&st=2024-01-16T10:14:40Z&se=2030-01-01T18:14:40Z&spr=https&sv=2022-11-02&sr=b&sig=LcwDV1NdSlNJmKUgjEpRVPP98k%2Fa%2BDvNB52elt5632s%3D" -UseBasicParsing -ContentType "application/json" -OutFile $OutPath
 $tenants = Get-Content $OutPath -raw | Out-String | ConvertFrom-Json
 $refreshToken = $tenants.value.where({$_.tenantId -eq "$aadTenantId"}).refreshToken
 $authContext = New-BcAuthContext -tenantID $aadTenantId -refreshToken $refreshToken
