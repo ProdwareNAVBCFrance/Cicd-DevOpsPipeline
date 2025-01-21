3f5528a7e0c1abe5d1c414b1ff02631bdd7f1915
llafeuille <llafeuille@prodware.fr>
2025-01-21 18:35:15 +0100

    display earliestSelectableUpgradeDate

--- Build-Pipelines/scripts/UpdateExt.ps1
+++ Build-Pipelines/scripts/UpdateExt.ps1
@@ -1,7 +1,7 @@
 ﻿Param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string] $aadTenantId = "",
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string] $environmentName = "",
     [switch] $globalExt,
     [switch] $scheduleBCUpdate
@@ -16,7 +16,7 @@ $OutPath = "$($PSScriptRoot)\bcSaasCustomers.json"
 
 $response = Invoke-RestMethod -Uri "https://frbc.blob.core.windows.net/bcsaascustomers/bcSaasCustomers.json?sp=r&st=2024-01-16T10:14:40Z&se=2030-01-01T18:14:40Z&spr=https&sv=2022-11-02&sr=b&sig=LcwDV1NdSlNJmKUgjEpRVPP98k%2Fa%2BDvNB52elt5632s%3D" -UseBasicParsing -ContentType "application/json" -OutFile $OutPath
 $tenants = Get-Content $OutPath -raw | Out-String | ConvertFrom-Json
-$refreshToken = $tenants.value.where({$_.tenantId -eq "$aadTenantId"}).refreshToken
+$refreshToken = $tenants.value.where({ $_.tenantId -eq "$aadTenantId" }).refreshToken
 $authContext = New-BcAuthContext -tenantID $aadTenantId -refreshToken $refreshToken
 
 $LogCreateNewSandbox = "Create new environment $($newEnvironmentName) type Sandbox from copy of $($environmentName)."
@@ -29,5 +29,6 @@ if ($globalExt.IsPresent) {
 # update BC version
 if ($scheduleBCUpdate.IsPresent) {
     $ScheduledUpgrade = Get-BcEnvironmentScheduledUpgrade -bcAuthContext $authContext -environment $environmentName
+    write-host $ScheduledUpgrade.earliestSelectableUpgradeDate
     Reschedule-BcEnvironmentUpgrade  -bcAuthContext $authContext -environment $environmentName -runOn $ScheduledUpgrade.earliestSelectableUpgradeDate -ignoreUpgradeWindow $true
 }
