5e2459fdf88daa23df7de1394ecf30d9f7733d62
ProdwareNAVBCFrance <82646058+ProdwareNAVBCFrance@users.noreply.github.com>
2025-01-21 15:37:22 +0100

    Update UpdateEnv.ps1

--- Build-Pipelines/scripts/UpdateEnv.ps1
+++ Build-Pipelines/scripts/UpdateEnv.ps1
@@ -19,20 +19,11 @@ $tenants = Get-Content $OutPath -raw | Out-String | ConvertFrom-Json
 $refreshToken = $tenants.value.where({$_.tenantId -eq "$aadTenantId"}).refreshToken
 $authContext = New-BcAuthContext -tenantID $aadTenantId -refreshToken $refreshToken
 
-# Delete environment
-$LogDeleteSandbox = "Delete $($newEnvironmentName) sandbox."
-Write-Host $LogDeleteSandbox
-
-try
-{
-# Remove the environment and wait for the deletion to complete
-Remove-BcEnvironment -bcAuthContext $authContext -environment $newEnvironmentName -doNotWait:$false
-}
-catch{ Write-host $_.Exception.Message } 
 $LogCreateNewSandbox = "Create new environment $($newEnvironmentName) type Sandbox from copy of $($environmentName)."
 Write-Host $LogCreateNewSandbox
 
 # copy environment
-Copy-BcEnvironment -bcAuthContext $authContext -environment $newEnvironmentName -sourceEnvironment $environmentName -doNotWait:$false
+# switch true delete the previous environment if present
+Copy-BcEnvironment -bcAuthContext $authContext -environment $newEnvironmentName -sourceEnvironment $environmentName -force:$true -doNotWait:$false
 
 
