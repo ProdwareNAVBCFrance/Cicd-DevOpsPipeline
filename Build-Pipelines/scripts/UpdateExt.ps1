30fd728d465b7bb4e12f39069cbe880c4704f27b
ProdwareNAVBCFrance <82646058+ProdwareNAVBCFrance@users.noreply.github.com>
2025-01-21 17:02:18 +0100

    Create UpdateExt.ps1

---
+++ Build-Pipelines/scripts/UpdateExt.ps1
@@ -0,0 +1,28 @@
+﻿Param(
+    [Parameter(Mandatory=$false)]
+    [string] $aadTenantId = "",
+    [switch] $globalExt,
+    [switch] $scheduleBCUpdate
+)
+Write-Host $aadTenantId
+Write-Host $newEnvironmentName
+Write-Host $environmentName
+
+
+$OutPath = "$($PSScriptRoot)\bcSaasCustomers.json"
+
+$response = Invoke-RestMethod -Uri "https://frbc.blob.core.windows.net/bcsaascustomers/bcSaasCustomers.json?sp=r&st=2024-01-16T10:14:40Z&se=2030-01-01T18:14:40Z&spr=https&sv=2022-11-02&sr=b&sig=LcwDV1NdSlNJmKUgjEpRVPP98k%2Fa%2BDvNB52elt5632s%3D" -UseBasicParsing -ContentType "application/json" -OutFile $OutPath
+$tenants = Get-Content $OutPath -raw | Out-String | ConvertFrom-Json
+$refreshToken = $tenants.value.where({$_.tenantId -eq "$aadTenantId"}).refreshToken
+$authContext = New-BcAuthContext -tenantID $aadTenantId -refreshToken $refreshToken
+
+$LogCreateNewSandbox = "Create new environment $($newEnvironmentName) type Sandbox from copy of $($environmentName)."
+Write-Host $LogCreateNewSandbox
+
+# update global ext.
+if ($globalExt.IsPresent) {
+}
+
+# update BC version
+if ($scheduleBCUpdate.IsPresent) {
+}
