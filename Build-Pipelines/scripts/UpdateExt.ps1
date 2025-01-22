108ab27b4b21777dde3b7a2c4b2ea16ca590ebb4
ProdwareNAVBCFrance <82646058+ProdwareNAVBCFrance@users.noreply.github.com>
2025-01-22 12:11:11 +0100

    Update UpdateExt.ps1

--- Build-Pipelines/scripts/UpdateExt.ps1
+++ Build-Pipelines/scripts/UpdateExt.ps1
@@ -14,6 +14,7 @@ Write-Host $scheduleBCUpdate
 
 $applicationFamily = "BusinessCentral"
 $apiVersion = "v2.6"
+$ignoreUpgradeWindow = -not $useEnvironmentUpdateWindow
 $OutPath = "$($PSScriptRoot)\bcSaasCustomers.json"
 
 $response = Invoke-RestMethod -Uri "https://frbc.blob.core.windows.net/bcsaascustomers/bcSaasCustomers.json?sp=r&st=2024-01-16T10:14:40Z&se=2030-01-01T18:14:40Z&spr=https&sv=2022-11-02&sr=b&sig=LcwDV1NdSlNJmKUgjEpRVPP98k%2Fa%2BDvNB52elt5632s%3D" -UseBasicParsing -ContentType "application/json" -OutFile $OutPath
@@ -73,9 +74,9 @@ if ($scheduleBCUpdate.IsPresent) {
     $ScheduledUpgrade = Get-BcEnvironmentScheduledUpgrade -bcAuthContext $authContext -environment $environmentName
     if ($ScheduledUpgrade) {
         $UpdateWindow = Get-BcEnvironmentUpdateWindow  -bcAuthContext $authContext -environment $environmentName
-        #Reschedule-BcEnvironmentUpgrade  -bcAuthContext $authContext -environment $environmentName -runOn $UpdateWindow.preferredStartTimeUtc -ignoreUpgradeWindow $true
-        $dateTime = (Get-Date).AddMinutes(1)
-        Reschedule-BcEnvironmentUpgrade  -bcAuthContext $authContext -environment $environmentName -runOn $dateTime -ignoreUpgradeWindow $true
+        Reschedule-BcEnvironmentUpgrade  -bcAuthContext $authContext -environment $environmentName -runOn $UpdateWindow.preferredStartTimeUtc -ignoreUpgradeWindow $ignoreUpgradeWindow
+        #$dateTime = (Get-Date).AddMinutes(1)
+        #Reschedule-BcEnvironmentUpgrade  -bcAuthContext $authContext -environment $environmentName -runOn $dateTime -ignoreUpgradeWindow $true
     }
     else {
         $Log = "Environment $($newEnvironmentName) is up to date."
